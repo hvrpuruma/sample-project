c5c09100239730367bf5b8ca1b64425be1a0fc17
Harsha <harsha@Harshas-MacBook-Air.local>
2023-05-06 19:02:12 -0500

    Updated port and added hello message in hello controller

--- src/main/java/com/harsha/sampleproject/controllers/HelloController.java
+++ src/main/java/com/harsha/sampleproject/controllers/HelloController.java
@@ -1,12 +1,15 @@
 package com.harsha.sampleproject.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    public String getResponse() {
-        return "Hello";
+    @RequestMapping("/hello")
+    public String getResponse(@RequestParam(value = "name", defaultValue = "People") String name) {
+        return "Hello " + name;
     }
 
 }
